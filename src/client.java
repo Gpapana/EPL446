97aa285ed256f65ac18a3159bdccbf4592f48311
PAPA_G <gpapan03@cs.ucy.ac.cy>
2017-11-16 18:34:53 +0200

    FIRST COMMIT

---
+++ src/client.java
@@ -0,0 +1,46 @@
+
+public class client extends Thread{
+
+	//////////////////////////////////////////////////////////////////////
+	
+	public void run(){
+		Database.threadNum++;
+		int id=Database.threadNum;
+		int ts=0;
+		
+		for (int i=0; i<2000; i++){
+			int p=i;
+		}
+		
+		for (int i=0; i<Database.actions[id-1].length; i++){
+			ts=print(ts, id, Database.actions[id-1][i], i);
+			if (i%10==0){
+				yield();
+			}
+		}
+		
+	}
+	
+	///////////////////////////////////////////////////////////////////////
+	
+	synchronized static int print (int ts, int id, String s, int i){
+		String[] par = new String[4];
+		if (s==null){
+			return ts;
+		}
+		par = s.split(" ");
+		
+		Database.timestamp++;
+		if (i==0){
+			ts=Database.timestamp;
+		}
+		
+		System.out.print(Database.timestamp+" cliend"+id+" T"+id+"="+ts+" ");
+		for (int j=0; j<par.length; j++){
+			System.out.print(par[j]+" ");
+		}
+		System.out.println();
+		
+		return ts;
+	}
+}
